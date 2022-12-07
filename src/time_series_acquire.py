7dab69b63a7e3d1957b62af2354246fcec3a28fe
Chris Rosenberger <jcrosenberger@pm.me>
2022-12-07 11:39:46 -0600

    Finished time_series_prepare lesson

--- src/time_series_acquire.py
+++ src/time_series_acquire.py
@@ -1,8 +1,14 @@
 import os
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from src.env import user, password, CodeUp_sql_server
 
+
+########################################################
+##############   Acquire DataFrames       ##############
+########################################################
 
 
 def get_CodeUp_db_url(database):
@@ -13,7 +19,36 @@ def get_CodeUp_db_url(database):
     return f'mysql+pymysql://{user}:{password}@{host}/{database}'
 
 
-def get_store_data():
+def set_index(df, index_variable):
+    try:
+        df[index_variable] = pd.to_datetime(df[index_variable])
+    except ValueError as e:
+        print('ValueError', e)
+    df = df.set_index(index_variable).sort_index() 
+
+    return df
+
+def get_german_power():
+    '''
+    Returns a dataframe which contains the building of new, invogue forms of power Germany has built. 
+    The forms of power include Wind, Solar, and 'wind plus solar' contrasted with the power consumption
+    of the country
+    '''
+
+
+########################################################
+#######     Simulated Store Data Functions       #######
+########################################################
+
+def get_store_df():
+    df = wrangle_store_data()
+    df = set_index(df, 'sale_date')
+    df = prep_store_data(df)
+
+    return df
+
+
+def query_for_store_data():
     '''
     Returns a dataframe of all store data in the tsa_item_demand database and saves a local copy as a csv file.
     '''
@@ -25,7 +60,7 @@ def get_store_data():
     '''
     
     df = pd.read_sql(query, get_CodeUp_db_url('tsa_item_demand'))
-    df.to_csv('data/tsa_item_demand.csv', index=False)
+    df.to_csv('data/tsa_item_data.csv', index=False)
     
     return df
 
@@ -36,11 +71,75 @@ def wrangle_store_data():
     '''
     filename = 'tsa_store_data.csv'
     
-    #if os.path.isfile('data/simple_wrangled_zillow_2017.csv'):
 
     if os.path.isfile(f'data/{filename}'):
-        df = pd.read_csv(f'data/{filename}')
+        df = pd.read_csv(f'data/{filename}', index_col = 0)
     else:
-        df = get_store_data()
+        df = query_for_store_data()
         
     return df
+
+
+def prep_store_data(df):
+    #df = set_index(wrangle_store_data(), 'sale_date')
+    df['month'] = df.index.month_name()
+    df['day_of_week'] = df.index.day_name()
+    df['sales_total'] = df.sale_amount * df.item_price
+    return df
+
+
+
+########################################################
+#######     German Green Energy DataFrame        #######
+########################################################
+
+def get_german_power():
+    df = acquire_german_power()
+    df = german_power_columns(df)
+    df = set_index(df, 'date')
+
+    return df
+
+
+
+def acquire_german_power():
+    '''
+    Returns a dataframe which contains the building of new, invogue forms of power Germany has built. 
+    The forms of power include Wind, Solar, and 'wind plus solar' contrasted with the power consumption
+    of the country
+    '''
+    filename = 'german_power.csv'
+
+    if os.path.isfile(f'data/{filename}'):
+        df = pd.read_csv(f'data/{filename}', index_col = 0)
+    
+    else:
+        df = pd.read_csv('https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv')
+        df.to_csv(f'data/{filename}')
+    
+    return df 
+
+def german_power_columns(df):
+    '''
+    Acquires the german power data frame. Changes column names and returns the dataframe
+    '''
+    df = df.rename(columns = {
+        'Date':'date',
+        'Consumption':'consumption',
+        'Wind':'wind',
+        'Solar':'solar',
+        'Wind+Solar':'wind_solar'
+    })
+
+    return df 
+
+
+###############################################################
+#######  Plot distributions of columns given dataframe  #######
+###############################################################
+
+def plot_distributions(df):
+    for col in list(df.columns):
+        plt.figure()
+        sns.histplot(df[col])
+        plt.title('Distribution of {}'.format(col))
